68c24ff494b2566dc3d2242736354a2e69520970
Maria <nikmaria15@gmail.com>
2022-12-14 16:34:53 +0500

    initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,17 @@
+import heroSorting from '../app';
+
+test('should be sorted', () => {
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const arr = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = heroSorting(arr);
+
+  expect(result).toEqual(expected);
+});
