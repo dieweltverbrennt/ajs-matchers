a450835c2530b1613b60b26651d072460a6c359b
Maria <nikmaria15@gmail.com>
2022-12-15 09:05:29 +0500

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 export default function heroSorting(arr) {
-  arr.every((element) => (element.name !== undefined || element.health !== undefined || typeof element.health !== 'string'));
   const sortedArr = arr.sort((a, b) => b.health - a.health);
   return sortedArr;
 }
